e962c09789b28db2fcc895c0dc13507cec8989d1
Thomas Hunt <thomas.a.hunt.4@gmail.com>
2025-07-13 17:00:36 +0200

    translation dictionary structure start

---
+++ features/language_select.py
@@ -0,0 +1,20 @@
+
+
+TRANSLATION = {
+    "en":{
+        "greeting": "hello"
+    },
+    "es":{
+        "greeting": "buenas"
+    },
+    "hi":{
+        "greeting": "something Hindi"
+    }
+}
+
+
+def language_selector(TRANSLATION):
+    input()
+
+
+
